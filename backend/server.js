d7386ee057357950065f684429acd50fd07b1283
dev-alfaiz <captainalfaiz@gmail.com>
2022-08-23 13:01:04 +0530

    Initial push

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+
+//Config
+dotenv.config({ path: "config/config.env" });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
